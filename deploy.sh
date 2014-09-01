374b5ed3803953c95c3e533ec41b9af8ee3ceae8
James <james.groves@ncas.ac.uk>
2014-09-01 10:33:15 +0100

    deploy script for the gps logger
    This script installs all the packages needed to setup the GPS logger

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Script to create directories and install packages required for the GPS PI logger
+cd /home/pi
+mkdir -p /opt/scripts
+
+#install packages vim gpsd and gpsd-clients 
+apt-get install -y vim gpsd gpsd gpsd-clients
+
+#Grab sirfdump from git make and create a symlink to in /usr/bin
+
+git clone https://github.com/illarionov/sirfdump
+cd sifrdump
+make
+sudo ln -sf /home/pi/sirfdump/sirfdump /usr/bin/sirfdump
+
+
+
+
+
+
+
+
+
+
+
