8b6de818eb85bb517f19914b169e3c13ad64f194
James <james.groves@ncas.ac.uk>
2014-09-01 18:04:53 +0100

    packages added to install script
    packages minicom screen and ntpdate added
    also a new directory created

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,10 @@
 #Script to create directories and install packages required for the GPS PI logger
 cd /home/pi
 mkdir -p /opt/scripts
+mkdir -p /home/pi/gpslog
 
 #install packages vim gpsd and gpsd-clients 
-apt-get install -y vim gpsd gpsd gpsd-clients
+apt-get install -y vim gpsd gpsd gpsd-clients minicom screen ntpdate 
 
 #Grab sirfdump from git make and create a symlink to in /usr/bin
 
