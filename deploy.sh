8433f40a317bbcad7c59a8a96bff1b679acc7aa4
James <james.groves@ncas.ac.uk>
2014-09-01 10:40:12 +0100

    typo on directory name

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ apt-get install -y vim gpsd gpsd gpsd-clients
 #Grab sirfdump from git make and create a symlink to in /usr/bin
 
 git clone https://github.com/illarionov/sirfdump
-cd sifrdump
+cd sirfdump
 make
 sudo ln -sf /home/pi/sirfdump/sirfdump /usr/bin/sirfdump
 
